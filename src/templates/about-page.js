6ac7e9db922b0f671d57700adc38ae727461b916
BlaxicanAvenger <41927782+BlaxicanAvenger@users.noreply.github.com>
2019-06-21 20:32:03 -0700

    Update about-page.js

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export const AboutPageTemplate = ({ title, description, content, fullImage, contentComponent }) => {
   const PageContent = contentComponent || Content
@@ -11,7 +10,7 @@ export const AboutPageTemplate = ({ title, description, content, fullImage, cont
   return (
     <div>
       <section className="bg-dark">
-        <img src={{${!!image.childImageSharp ? image.childImageSharp.fluid.src : image }}} alt={title} className="bg-image opacity-50" />
+      <PreviewCompatibleImage imageInfo={fullImage} className="bg-image opacity-50" />
         <div className="container height-lg-30">
           <div className="row">
             <div className="col-md-8 col-lg-7 col-xl-6">
